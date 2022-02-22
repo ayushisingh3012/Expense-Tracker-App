a1b30080c36b0d513d339e01e461004d5142c395
geetika1723 <geetikasethi1723@gmail.com>
2022-02-22 18:46:14 +0530

    4th vedio 1st part commit

--- app/src/main/java/com/example/expensetrackerapp/RegistrationActivity.java
+++ app/src/main/java/com/example/expensetrackerapp/RegistrationActivity.java
@@ -3,12 +3,47 @@ package com.example.expensetrackerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private  EditText mPass;
+    private Button btnReg;
+    private TextView mSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+    private void  registration()
+    {
+        mEmail=findViewById(R.id.email_reg);
+        mPass=findViewById(R.id.password_reg);
+        btnReg=findViewById(R.id.btn_reg);
+        mSignin=findViewById(R.id.signin_here);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=mEmail.getText().toString().trim();
+                String pass=mPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email Required ......");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass))
+                {
+                    mPass.setError("Password Required......");
+                    return;
+                }
+            }
+        });
     }
 }
